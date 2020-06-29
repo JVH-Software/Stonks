ac652558fcfa73b8ca05f70ba0d7d7c1af735484
Justin van Heek <justin.vanheek@vhsoftware.dev>
2020-06-28 18:50:52 -0700

    Updated docstrings and readme

--- plugins/base.py
+++ plugins/base.py
@@ -28,6 +28,16 @@ class YahooFinancePlugin(Plugin):
         Dividends: Dividends paid
 
         Stock Splits: Stock splits
+
+        Open Change: Relative change in Open value from previous day
+
+        High Change: Relative change in High value from previous day
+
+        Low Change: Relative change in Low value from previous day
+
+        Close Change: Relative change in Close value from previous day
+
+        Volume Change: Relative change in Volume value from previous day
     """
 
     available_keys = {"Open", "High", "Low", "Close", "Volume", "Dividends", "Stock Splits",
@@ -125,13 +135,15 @@ class GoogleTrendsPlugin(Plugin):
     Stonks plugin for Google Trends.
 
     Keys:
-        Monthly Relative Interest
+        Monthly Relative Interest: Google Trends Interest value relative to the past 30 days.
 
-        Annual Relative Interest
+        Annual Relative Interest: Google Trends Interest value relative to the past 365 days.
 
-        Monthly Relative Interest To S&P
+        Monthly Relative Interest To S&P: Google Trends Interest value relative to "S&P" Interest value within the past
+        30 days.
 
-        Annual Relative Interest To S&P
+        Annual Relative Interest To S&P: Google Trends Interest value relative to "S&P" Interest value within the past
+        365 days.
     """
 
     available_keys = {"Monthly Relative Interest", "Annual Relative Interest", "Monthly Relative Interest To S&P",
--- readme.md
+++ readme.md
@@ -4,12 +4,38 @@ A python package for accessing historical stock data. This library was designed 
 
 ## Available Keywords From Base Plugins
 
-* Open
-* High
-* Low
-* Close
-* Volume
-* Dividends
-* Stock Splits
+`Open` Opening price.
+
+`High` Highest price.
+
+`Low` Lowest price.
+
+`Close` Closing price.
+
+`Volume` Trading volume.
+
+`Dividends` Dividends paid.
+
+`Stock Splits` Stock splits.
+
+`Open Change` Relative change in Open value from previous day.
+
+`High Change` Relative change in High value from previous day.
+
+`Low Change` Relative change in Low value from previous day.
+
+`Close Change` Relative change in Close value from previous day.
+
+`Volume Change` Relative change in Volume value from previous day.
+
+`Monthly Relative Interest` Google Trends Interest value relative to the past 30 days.
+
+`Annual Relative Interest` Google Trends Interest value relative to the past 365 days.
+
+`Monthly Relative Interest To S&P` Google Trends Interest value relative to "S&P" Interest value within the past
+30 days.
+
+`Annual Relative Interest To S&P` Google Trends Interest value relative to "S&P" Interest value within the past
+365 days.
 
 ## Future Work
